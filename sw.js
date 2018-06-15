694e80d2f8310df9aa20c18cea8086e119f2aac7
Marina <musicbymarina@gmail.com>
2018-06-15 02:26:24 +0200

    Modify service worker for stage 2

--- sw.js
+++ sw.js
@@ -2,19 +2,64 @@
 * @description Set a name for the current cache and Default files to always cache
 *
 */
-const cacheName = 'v1';
+const cacheName = 'mws-v3';
+
+const imagesToCache = [
+'/img/1.jpg',
+'/img/2.jpg',
+'/img/3.jpg',
+'/img/4.jpg',
+'/img/5.jpg',
+'/img/6.jpg',
+'/img/7.jpg',
+'/img/8.jpg',
+'/img/9.jpg',
+'/img/10.jpg',
+'/img/1_small.jpg',
+'/img/2_small.jpg',
+'/img/3_small.jpg',
+'/img/4_small.jpg',
+'/img/5_small.jpg',
+'/img/6_small.jpg',
+'/img/7_small.jpg',
+'/img/8_small.jpg',
+'/img/9_small.jpg',
+'/img/10_small.jpg',
+'/img/1.webp',
+'/img/2.webp',
+'/img/3.webp',
+'/img/4.webp',
+'/img/5.webp',
+'/img/6.webp',
+'/img/7.webp',
+'/img/8.webp',
+'/img/9.webp',
+'/img/10.webp',
+'/img/1_small.webp',
+'/img/2_small.webp',
+'/img/3_small.webp',
+'/img/4_small.webp',
+'/img/5_small.webp',
+'/img/6_small.webp',
+'/img/7_small.webp',
+'/img/8_small.webp',
+'/img/9_small.webp',
+'/img/10_small.webp',
+];
+
 const cacheFiles = [
   '/',
   'index.html',
   'restaurant.html',
-  './js/indexController.js',
+  './js/dbhelper.js',
   './js/main.js',
+   './js/idb.js',
   './js/restaurant_info.js',
+   './js/IndexController.js',
   './css/styles.css',
-  './data/restaurants.json',
-  './img/',
   'manifest.json',
-  'icon.png'
+  'icon.png',
+  ...imagesToCache
 ];
 
 /**
@@ -45,11 +90,14 @@ self.addEventListener('activate', event => {
 
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return Promise.all(cacheNames.map(thisCacheName => {
-        if (thisCacheName !== cacheName) {
+      return Promise.all(
+      cacheNames
+      .filter(thisCacheName => {
+        return (thisCacheName.startsWith('mws-') && !thisCacheName.includes(cacheName));
+      })
+      .map(thisCacheName => {
           console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
           return caches.delete(thisCacheName);
-        }
       }));
     })
   );
@@ -67,11 +115,20 @@ self.addEventListener('fetch', event => {
   console.log('[ServiceWorker] Fetch event now', event.request.url);
 
   event.respondWith(
-    caches.open(cacheName).then(cache => {
-      return cache.match(event.request).then(response => {
+    caches.open(cacheName)
+    .then(cache => {
+      return cache.match(event.request)
+      .then(response => {
         console.log("[ServiceWorker] Found in Cache", event.request.url, response);
-        return response || fetch(event.request).then(response => {
-          console.log('[ServiceWorker] not Found in Cache, need to search in the network', event.request.url);
+            return response || fetch(event.request).then(response => {
+            console.log('[ServiceWorker] not Found in Cache, need to search in the network', event.request.url);
+              if (response.url.includes('.jpg') || response.url.includes('.webp')) {
+                if (response.url.includes(location.origin)) {
+                  cache.put(event.request, response.clone());
+                  console.log('[ServiceWorker] New images Cached', event.request.url);
+                  return response;
+                }
+              }
           cache.put(event.request, response.clone());
           console.log('[ServiceWorker] New Data Cached', event.request.url);
           return response;
